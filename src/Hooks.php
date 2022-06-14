42fa1a6cf7b3f574ec9ddc1effcaf4c6eeffea76
Mathias <mathiasmun@gmail.com>
2022-06-14 22:56:31 +0200

    Setup

--- src/Hooks.php
+++ src/Hooks.php
@@ -1,10 +1,18 @@
 <?php
 
-
 namespace Morningtrain\WP\Hooks;
-
 
 class Hooks
 {
+    public static bool $foo = false;
 
+    public static function foo()
+    {
+        static::$foo = true;
+    }
+
+    public static function bar() : string
+    {
+        return "bar";
+    }
 }
--- tests/Unit/DummyTest.php
+++ tests/Unit/DummyTest.php
@@ -3,3 +3,13 @@
 it('has Hook main class', function () {
     expect(\Morningtrain\WP\Hooks\Hooks::class)->toBeString();
 });
+
+it('can bar', function(){
+    expect(\Morningtrain\WP\Hooks\Hooks::bar())->toBeString();
+});
+
+it('can foo', function(){
+    expect(\Morningtrain\WP\Hooks\Hooks::$foo)->toBeFalse();
+    \Morningtrain\WP\Hooks\Hooks::foo();
+    expect(\Morningtrain\WP\Hooks\Hooks::$foo)->toBeTrue();
+});
